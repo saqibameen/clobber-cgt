0f557aec9c3fcd75e7b58c1149010dafd57980ee
mirzaabdulwahab1612 <awahab.bscs16seecs@seecs.edu.pk>
2022-02-03 09:41:06 -0700

    Input Output Time checks made consistent with requirements

--- boolean_negamax.py
+++ boolean_negamax.py
@@ -5,19 +5,34 @@
 import time
 from game_basics import colorAsString, isBlackWhite, opponent
 
-def negamaxBoolean(state):
+
+win_move = None
+node_count = 0
+start = time.process_time()
+
+def negamaxBoolean(state, time_limit):
+    global win_move, node_count, start
+    node_count += 1
+
     if state.endOfGame():
-        return state.staticallyEvaluateForToPlay()
+        return state.staticallyEvaluateForToPlay(), win_move
     for m in state.legalMoves():
         state.play(m)
-        success = not negamaxBoolean(state)
-        state.undoMove()
-        if success:
-            return True
-    return False
+        success = not negamaxBoolean(state, time_limit)[0]
+        timeUsed = time.process_time() - start
+        if(timeUsed >= time_limit):
+            win_move = None
+            return None, None
+        else:
+            state.undoMove()
+            if success:
+                win_move = m
+                return True, win_move
+    return False, None
 
-def timed_solve(state): 
+def timed_solve(state, time_limit): 
+    global start
     start = time.process_time()
-    win = negamaxBoolean(state)
+    win, m = negamaxBoolean(state, time_limit)
     timeUsed = time.process_time() - start
-    return win, timeUsed
+    return win, m, timeUsed, node_count
--- main.py
+++ main.py
@@ -5,14 +5,29 @@
 from game_basics import BLACK, WHITE, EMPTY, colorAsString
 from clobber_1d import Clobber_1d
 from boolean_negamax import timed_solve
+import sys
 
-def solveAndPrint(state, solver): 
+
+
+def solveAndPrint(state, player, time_limit, solver): 
     print("Solve Board: size", len(state.board))
     state.print()
     print(colorAsString(state.toPlay), "to play")
-    isWin, timeUsed = solver(state)
-    print("Win: {}\nTime used: {:.4f}\n".format(
-        isWin, timeUsed))
+    isWin, win_move, timeUsed, node_count = solver(state, time_limit)
+
+    if(isWin == None):
+        isWin = "?"
+    
+    elif(player == BLACK and isWin):
+        isWin = "B"
+    elif(player == BLACK and not isWin):
+        isWin = "W"
+    elif(player == WHITE and isWin):
+        isWin = "W"
+    elif(player == WHITE and not isWin):
+        isWin = "B"
+
+    print("{} {} {:.4f} {}\n".format(isWin, win_move, timeUsed, node_count))
 
 def testGames(solver):
     for i in range(2,25):
@@ -21,7 +36,7 @@ def testGames(solver):
             solveAndPrint(game, solver)
 
 def analyze655Variation(solver):
-    game = Clobber_1d("BWBWBWB.WW", BLACK)
+    game = Clobber_1d("BWBWBWB.WW", WHITE)
     solveAndPrint(game, solver)
 
 def analyze655Game(solver):# Analyze the game we played in class
@@ -36,7 +51,33 @@ def analyze655Game(solver):# Analyze the game we played in class
     game.play((8, 9))
     solveAndPrint(game, solver)
 
-testGames(timed_solve)
 
-#analyze655Game(timed_solve)
-#analyze655Variation(timed_solve)
+
+def main(board, player, time_limit, timed_solve):
+    # testGames(timed_solve)
+    #analyze655Game(timed_solve)
+    # analyze655Variation(timed_solve)
+
+    game = Clobber_1d(board, player)
+    solveAndPrint(game, player, time_limit, timed_solve)
+
+
+if __name__ == "__main__":
+
+    if(len(sys.argv) == 4):
+        board = str(sys.argv[1]).upper()
+        player = str(sys.argv[2]).upper()
+        time_limit = float(sys.argv[3])
+
+        assert player == "B" or player == "W"
+
+        if(player == "B"):
+            player = BLACK
+        else:
+            player = WHITE
+
+        # Playing the game, main 
+        main(board, player, time_limit, timed_solve)
+    else:
+        print(f"Misisng Arguments")
+
