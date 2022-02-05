5e6bbb4fc6cc958fc766bb91f7e72eb3091e2b8f
Kamran Janjua <kamranejaz98@gmail.com>
2022-02-05 06:16:27 -0700

    Added comparison between hash and no hash tt

--- main_tt.py
+++ main_tt.py
@@ -1,13 +1,16 @@
 from clobber_1d import Clobber_1d
 from game_basics import BLACK, WHITE, colorAsString
 from transposition_table_simple import TranspositionTable
-from boolean_negamax_tt import timed_solve
+from boolean_negamax_tt import timed_solve as timed_solve_hash
+from negamax_no_hash import timed_solve as timed_solve_no_hash
 import time
 import sys
 
+mode = "test" # run or test
+
 def test_solve_with_tt(state, player, time_limit, board):
     tt = TranspositionTable()
-    isWin, win_move, timeUsed, node_count =  timed_solve(state, tt, time_limit, board)
+    isWin, win_move, timeUsed, node_count =  timed_solve_hash(state, tt, time_limit, board)
 
     if(isWin == None):
         isWin = "?"
@@ -16,24 +19,67 @@ def test_solve_with_tt(state, player, time_limit, board):
     elif((player == BLACK and not isWin) or (player == WHITE and isWin)):
         isWin = "W"
 
-    print("{} {} {:.64f} {}\n".format(isWin, win_move, timeUsed, node_count))
+    result = "{} {}".format(isWin, win_move)
+    return result, timeUsed
 
-if __name__ == "__main__":
+def test_solve_with_tt_no_hash(state, player, time_limit, board):
+    tt = TranspositionTable()
+    isWin, win_move, timeUsed, node_count =  timed_solve_no_hash(state, tt, time_limit, board)
 
-    if(len(sys.argv) == 4):
-        board = str(sys.argv[1]).upper()
-        player = str(sys.argv[2]).upper()
-        time_limit = float(sys.argv[3])
+    if(isWin == None):
+        isWin = "?"
+    elif((player == BLACK and isWin) or (player == WHITE and not isWin)):
+        isWin = "B"
+    elif((player == BLACK and not isWin) or (player == WHITE and isWin)):
+        isWin = "W"
 
-        assert player == "B" or player == "W"
+    result = "{} {}".format(isWin, win_move)
+    return result, timeUsed
 
-        if(player == "B"):
+def verify_results_zobrist_hash():
+    test_cases = open("testcases.txt", "r").readlines()
+    test_cases = [x.strip() for x in test_cases]
+    time_limit = 10 # base time limit, in seconds
+    for test_no, test_case in enumerate(test_cases):
+        board, player_char = test_case.split()
+        
+        if(player_char == "B"):
             player = BLACK
         else:
             player = WHITE
 
         state = Clobber_1d(board, player)
-        # Playing the game, main 
-        test_solve_with_tt(state, player, time_limit, board)
-    else:
-        print(f"Misisng Arguments")
+        result_hash, timeUsed_hash = test_solve_with_tt(state, player, time_limit, board)
+        result_no_hash, timeUsed_no_hash = test_solve_with_tt_no_hash(state, player, time_limit, board)
+
+        if result_hash != result_no_hash:
+            print(f"Test No. {test_no} | Board: {board} | Player to Play: {player_char}")
+            print("Result Hash: {} | Time Hash: {:.4f}".format(result_hash, timeUsed_hash))
+            print("Result No Hash: {} | Time No Hash: {:.4f}".format(result_no_hash, timeUsed_no_hash))
+            print()
+        else:
+            assert result_hash == result_no_hash
+            print("Test No. {} Passed! | Time Hash: {:.4f} | Time No Hash: {:.4f} | Board: {}".format(test_no, timeUsed_hash, timeUsed_no_hash, board))
+
+if __name__ == "__main__":
+
+    if mode == "run":
+        if(len(sys.argv) == 4):
+            board = str(sys.argv[1]).upper()
+            player = str(sys.argv[2]).upper()
+            time_limit = float(sys.argv[3])
+
+            assert player == "B" or player == "W"
+
+            if(player == "B"):
+                player = BLACK
+            else:
+                player = WHITE
+
+            state = Clobber_1d(board, player)
+            # Playing the game, main 
+            test_solve_with_tt(state, player, time_limit, board)
+        else:
+            print(f"Misisng Arguments")
+
+    else: verify_results_zobrist_hash() # do the sanity check
