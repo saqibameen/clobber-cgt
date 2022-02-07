c6895c656d68a247fe4674ea238ae1f2aa554c95
Saqib Ameen <mrsaqibameen@gmail.com>
2022-02-07 12:46:02 -0700

    remove writing to file

--- tests_suit.py
+++ tests_suit.py
@@ -9,6 +9,12 @@ from boolean_negamax_tt_ids_no_heuristic import timed_solve as ids_no_heuristic_
 import time
 import sys
 
+from clobber_1d_cy import Clobber_1d as Clobber_1dC
+from transposition_table_simple_cy import TranspositionTable as TTC
+# import boolean_negamax_tt_cy
+from boolean_negamax_tt_cy import timed_solve as timed_solve_hashC
+
+
 def test_solve_with_tt(state, player, time_limit, board, solver):
     tt = TranspositionTable()
     isWin, win_move, timeUsed, node_count =  solver(state, tt, time_limit, board)
@@ -21,25 +27,43 @@ def test_solve_with_tt(state, player, time_limit, board, solver):
         isWin = "W"
 
     # open logs file in append mode and create if does not exist.
-    logs = open("logs.txt", "a")
+    # logs = open("logs.txt", "a")
     # Write the solver name, isWin, win_move, time_used and the number of nodes visited.
-    logs.write("{} {} {:.64f} {}\n".format(isWin, win_move, timeUsed, node_count))
+    # logs.write("{} {} {:.64f} {}\n".format(isWin, win_move, timeUsed, node_count))
+
+    return isWin, win_move, timeUsed, node_count
+
+def test_solve_with_ttC(state, player, time_limit, board, solver):
+    tt = TTC()
+    isWin, win_move, timeUsed, node_count =  solver(state, tt, time_limit, board)
+    
+    if(isWin == None):
+        isWin = "?"
+    elif((player == BLACK and isWin) or (player == WHITE and not isWin)):
+        isWin = "B"
+    elif((player == BLACK and not isWin) or (player == WHITE and isWin)):
+        isWin = "W"
+
+    # open logs file in append mode and create if does not exist.
+    # logs = open("logs.txt", "a")
+    # Write the solver name, isWin, win_move, time_used and the number of nodes visited.
+    # logs.write("{} {} {:.64f} {}\n".format(isWin, win_move, timeUsed, node_count))
 
     return isWin, win_move, timeUsed, node_count
 
 def worst_case_testing():
     # Generate random board. With a random number of 'B','W','.'.
     heuristic_ids, zob_hash, mm = [], [], []
-    results = open("results.csv", "w")
-    results.write("IDS_heuristic,IDS_no_heuristic,Zobrist,Base")
-    results.write("\n")
-    for i in range(1, 41):
+    # results = open("results3.csv", "w")
+    # results.write("Cython, Board_Size")
+    # results.write("\n")
+    for i in range(1, 31):
         # BW pattern upto i + 1
         pairs = (i+1) // 2
         board = "BW" * pairs
         board =  board[:i]
 
-        print("board: ", board)
+        print("board: ", board, " size: ", i)
 
         # state = Clobber_1d(board, BLACK)
         # state2 = Clobber_1d(board, BLACK)
@@ -64,12 +88,15 @@ def worst_case_testing():
         state2 = Clobber_1d(board, WHITE)
         state3 = Clobber_1d(board, WHITE)
         state4 = Clobber_1d(board, WHITE)
+        state5 = Clobber_1dC(board, WHITE)
 
         isWin, win_move, timeUsed, node_count = test_solve_with_tt(state, WHITE, 100, board, zobrist_solver)
         isWin3, win_move3, timeUsed3, node_count3 = test_solve_with_tt(state3, WHITE, 100, board, ids_solver)
         isWin4, win_move4, timeUsed4, node_count4 = test_solve_with_tt(state4, WHITE, 100, board, ids_no_heuristic_solver)
+        isWin5, win_move5, timeUsed5, node_count5 = test_solve_with_ttC(state5, WHITE, 100, board, timed_solve_hashC)
         isWin2, win_move2, timeUsed2, node_count2 = test_solve_with_tt(state2, WHITE, 100, board, regular_hash_solver)
         
+
         if win_move3 == "depthReached" or win_move3 == "timelimitReached":
             win_move3 = None
 
@@ -77,6 +104,7 @@ def worst_case_testing():
             win_move4 = None
         
         print("[Zobrist] isWin: ", isWin, " win_move: ", win_move, " timeUsed: ", timeUsed, " node_count: ", node_count)
+        print("[Cython] isWin5: ", isWin5, " win_move5: ", win_move5, " timeUsed5: ", timeUsed5, " node_count5: ", node_count5)
         print("[IDS Heuristic] isWin3: ", isWin3, " win_move3: ", win_move3, " timeUsed3: ", timeUsed3, " node_count3: ", node_count3)
         print("[IDS No-Heuristic] isWin4: ", isWin4, " win_move4: ", win_move4, " timeUsed4: ", timeUsed4, " node_count4: ", node_count4)
         print("[MM] isWin2: ", isWin2, " win_move2: ", win_move2, " timeUsed2: ", timeUsed2, " node_count2: ", node_count2)
@@ -85,10 +113,10 @@ def worst_case_testing():
         if(isWin2 == "W" or isWin2 == "B" or (isWin != "W" and isWin != "B") or (isWin3 != "W" and isWin3 != "B") or (isWin4 != "W" and isWin4 != "B")):
             assert isWin == isWin2 == isWin3 == isWin4 and win_move == win_move2 == win_move3 == win_move4
 
-        results.write(f"{timeUsed3},{timeUsed4},{timeUsed},{timeUsed2},{len(board)}")
-        results.write("\n")
+        # results.write(f"{timeUsed5},{len(board)}")
+        # results.write("\n")
     
-    results.close()
+    # results.close()
 
 
 def random_test_suit_testing():
