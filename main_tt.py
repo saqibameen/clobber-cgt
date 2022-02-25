e91d94e9ef7b2dfe79a75c025ad3fd36389a4b58
mirzaabdulwahab1612 <awahab.bscs16seecs@seecs.edu.pk>
2022-02-25 01:12:19 -0700

    minor modifications

--- main_tt.py
+++ main_tt.py
@@ -1,25 +1,23 @@
 from clobber_1d import Clobber_1d
 from game_basics import BLACK, WHITE, colorAsString
 from transposition_table_simple import TranspositionTable
-from boolean_negamax_tt import timed_solve as timed_solve_hash
-from negamax_no_hash import timed_solve as timed_solve_no_hash
-from boolean_negamax_tt_ids import timed_solve as timed_solve_ids
+from cgt import timed_solve as cgt_timed_solve
 import time
 import sys
 
 mode = "test" # run or test
-player_to_char = {'.': 0,
-                  'B': 1,
-                  'W': 2}
 
-char_to_player = {0: '.',
-                  1: 'B',
-                  2: 'W'}
-
-def test_solve_with_tt(state, player, time_limit, board):
+def test_solve_with_cgt(state, player, time_limit, board):
     tt = TranspositionTable()
-    isWin, win_move, timeUsed, node_count =  timed_solve_hash(state, tt, time_limit, board)
+    isWin, win_move, timeUsed, node_count =  cgt_timed_solve(state, tt, time_limit, board)
 
+    # change win_move to from-to
+    if win_move is not None:
+        src, to = str(win_move).split(', ')
+        src = src.replace('(', '')
+        to = to.replace(')', '')
+        win_move = "{}-{}".format(src, to)
+        
     if(isWin == None):
         isWin = "?"
     elif((player == BLACK and isWin) or (player == WHITE and not isWin)):
@@ -33,44 +31,7 @@ def test_solve_with_tt(state, player, time_limit, board):
     result = "{} {}".format(isWin, win_move)
     return result, timeUsed
 
-def test_solve_with_tt_ids(state, player, time_limit, board):
-    tt = TranspositionTable()
-    isWin, win_move, timeUsed, node_count =  timed_solve_ids(state, tt, time_limit, board)
-    
-    if(isWin == None):
-        isWin = "?"
-    elif((player == BLACK and isWin) or (player == WHITE and not isWin)):
-        isWin = "B"
-    elif((player == BLACK and not isWin) or (player == WHITE and isWin)):
-        isWin = "W"
-    
-    if win_move == "depthReached" or win_move == "timelimitReached":
-            win_move = None
-
-    if mode == "run":
-        print("{} {} {:.4f} {}".format(isWin, win_move, timeUsed, node_count))
-    
-    result = "{} {}".format(isWin, win_move)
-    return result, timeUsed
-
-def test_solve_with_tt_no_hash(state, player, time_limit, board):
-    tt = TranspositionTable()
-    isWin, win_move, timeUsed, node_count =  timed_solve_no_hash(state, tt, time_limit, board)
-
-    if(isWin == None):
-        isWin = "?"
-    elif((player == BLACK and isWin) or (player == WHITE and not isWin)):
-        isWin = "B"
-    elif((player == BLACK and not isWin) or (player == WHITE and isWin)):
-        isWin = "W"
-
-    if mode == "run":
-        print("{} {} {:.4f} {}\n".format(isWin, win_move, timeUsed, node_count))
-
-    result = "{} {}".format(isWin, win_move)
-    return result, timeUsed
-
-def verify_results_zobrist_hash():
+def run_sanity_checks():
     test_cases = open("testcases.txt", "r").readlines()
     test_cases = [x.strip() for x in test_cases]
     time_limit = 10 # base time limit, in seconds
@@ -83,17 +44,8 @@ def verify_results_zobrist_hash():
             player = WHITE
 
         state = Clobber_1d(board, player)
-        result_hash, timeUsed_hash = test_solve_with_tt(state, player, time_limit, board)
-        result_ids, timeUsed_ids = test_solve_with_tt_ids(state, player, time_limit, board)
-
-        if result_hash != result_ids:
-            print(f"Test No. {test_no} | Board: {board} | Player to Play: {player_char}")
-            print("Result Hash: {} | Time Hash: {:.4f}".format(result_hash, timeUsed_hash))
-            print("Result IDS: {} | Time IDS: {:.4f}".format(result_ids, timeUsed_ids))
-            print()
-        else:
-            assert result_hash == result_ids
-            print("Test No. {} Passed! | Time Hash: {:.4f} | Time IDS {:.4f} | Board: {}".format(test_no, timeUsed_hash, timeUsed_ids, board))
+        result, timeUsed = test_solve_with_cgt(state, player, time_limit, board)
+        print(f"Result {result} | Time Used {timeUsed}")
 
 if __name__ == "__main__":
     if mode == "run":
@@ -110,9 +62,9 @@ if __name__ == "__main__":
 
             state = Clobber_1d(board, player)
             # Playing the game, main
-            test_solve_with_tt_ids(state, player, time_limit, board)
+            test_solve_with_cgt(state, player, time_limit, board)
             # test_solve_with_tt(state, player, time_limit, board)
         else:
             print(f"Misisng Arguments")
 
-    else: verify_results_zobrist_hash() # do the sanity check
+    else: run_sanity_checks() # do the sanity check
